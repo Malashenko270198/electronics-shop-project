3ddaf1b5116f6054a99fb8d78ce9219c43e572b0
Ruslan Malashenko <malashencko.ruslan@yandex.ru>
2023-07-23 07:48:00 +0300

    fix

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+assert 10000 * Item.pay_rate == item1.price
+assert item1.price == 10000
+
+assert 20000 * Item.pay_rate == item2.price
+assert item2.price == 20000
