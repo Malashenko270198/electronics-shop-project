7491dcdacb917d56d2c3401ea0fbf5edb0f9123d
Ruslan Malashenko <malashencko.ruslan@yandex.ru>
2023-07-24 05:48:42 +0300

    final_update

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,11 +1,12 @@
-import pytest
 from src.item import Item
 
 item1 = Item("Смартфон", 10000, 20)
 item2 = Item("Ноутбук", 20000, 5)
 
-assert 10000 * Item.pay_rate == item1.price
-assert item1.price == 10000
 
-assert 20000 * Item.pay_rate == item2.price
-assert item2.price == 20000
+def test_pay_rate():
+    assert 10000 * Item.pay_rate == item1.price
+    assert item1.price == 10000
+
+    assert 20000 * Item.pay_rate == item2.price
+    assert item2.price == 20000
